3bded78f99ee1c28b0fcbddde169c5d6c2bc59c4
SatoshiHaramura <satoshi.haramura@gmail.com>
2021-11-04 10:43:28 +0900

    フレーム生成およびポイント計算の改善

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,60 +1,38 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-MAX_FRAME = 10
 STRIKE = 10
 SPARE = 10
-STRIKE_MARK = -1
 
 score = ARGV[0]
 scores = score.split(',')
 
-shots = []
-scores.each do |s|
-  if s == 'X'
-    shots << STRIKE
-    shots << STRIKE_MARK
-  else
-    shots << s.to_i
-  end
-end
-
+frame = []
 frames = []
-shots.each_slice(2) do |s|
-  if frames.count < MAX_FRAME
-    frames << s
-  elsif s.count == 2
-    frames[-1] << s.shift
-    frames[-1] << s.pop
-  else
-    frames[-1] << s.shift
+scores.each do |s|
+  frame << (s == 'X' ? STRIKE : s.to_i)
+  # (9フレーム目まで) && (frameが既に2投分の情報を持つ || ストライクのとき)
+  if (frames.count < 9) && ((frame.count == 2) || (frame[0] == STRIKE))
+    frames << frame
+    frame = []
   end
 end
-
-frames.each do |frame|
-  frame.delete(STRIKE_MARK)
-end
-
-strike_frames = []
-spare_frames = []
-frames.each_with_index do |frame, i|
-  strike_frames << i if frame.count == 1
-  spare_frames << i if (frame.count == 2) && (frame.sum == SPARE)
-end
+# 10フレーム目としてスコアを格納
+frames << frame
 
 point = 0
-frames.each_with_index do |frame, i|
-  if strike_frames.include?(i)
+frames.each_with_index do |f, i|
+  if f.count == 1
     point += (STRIKE + frames[i + 1][0])
-    point += if strike_frames.include?(i + 1)
+    point += if frames[i + 1].count == 1
                frames[i + 2][0]
              else
                frames[i + 1][1]
              end
-  elsif spare_frames.include?(i)
+  elsif (f.count == 2) && (f.sum == SPARE)
     point += (SPARE + frames[i + 1][0])
   else
-    point += frame.sum
+    point += f.sum
   end
 end
 puts point
