092da3f3104acc490efa375ba01e2b1f8a0deadc
SatoshiHaramura <satoshi.haramura@gmail.com>
2021-11-11 10:07:21 +0900

    メソッドの分割およびインスタンス変数の削除およびカラムの確定ロジックの改善

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,33 +5,38 @@ MAX_COLUMNS = 3
 MAX_FILENAME_LENGTH = 24
 
 def main
-  retrieve
-  display
+  files = retrieve_files
+  number_of_columns = calculate_columns
+  number_of_rows = calculate_rows(files, number_of_columns)
+  display(files, number_of_rows, number_of_columns)
 end
 
-def retrieve
-  @files = Dir.glob('[^.]*')
-  @number_of_files = @files.count
+def retrieve_files
+  Dir.glob('[^.]*')
+end
+
+def calculate_columns
   number_of_terminal_columns = `tput cols`.to_i
-  @number_of_columns = if number_of_terminal_columns < MAX_FILENAME_LENGTH * 2
-                         1
-                       elsif number_of_terminal_columns < MAX_FILENAME_LENGTH * 3
-                         2
-                       else
-                         MAX_COLUMNS
-                       end
-  @number_of_rows = (@number_of_files.to_f / @number_of_columns).ceil(0)
+  return MAX_COLUMNS if number_of_terminal_columns > MAX_FILENAME_LENGTH * MAX_COLUMNS
+
+  1.upto(MAX_COLUMNS) do |column|
+    break column if number_of_terminal_columns < MAX_FILENAME_LENGTH * (column + 1)
+  end
 end
 
-def display
-  @number_of_rows.times do |row|
-    @number_of_columns.times do |column|
-      index = @number_of_rows * column + row
-      next unless @files[index]
+def calculate_rows(files, number_of_columns)
+  (files.count.to_f / number_of_columns).ceil(0)
+end
 
-      print @files[index].ljust(MAX_FILENAME_LENGTH)
-      puts "\n" if index >= (@number_of_files - @number_of_rows)
+def display(files, number_of_rows, number_of_columns)
+  number_of_rows.times do |row|
+    number_of_columns.times do |column|
+      index = number_of_rows * column + row
+      next unless files[index]
+
+      print files[index].ljust(MAX_FILENAME_LENGTH)
     end
+    puts "\n"
   end
 end
 
