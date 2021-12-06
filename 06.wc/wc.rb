bf0119a191803ce9394acd346cc1915cf4cc8877
SatoshiHaramura <satoshi.haramura@gmail.com>
2021-12-06 14:55:22 +0900

    mainメソッドの改善に伴い全体的に修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,16 +4,18 @@
 require 'optparse'
 
 def main
-  options, files = receive_argument
-  if files.count != 0
-    files_info = check_files_info(files)
-    display_files_info(options, files_info)
-    display_total(options, files_info) if files.count > 1
+  options, files_name = receive_argument
+
+  files_contents = []
+  if files_name.count != 0
+    files_contents = files_name.map { |name| File.read(name) }
   else
-    stdin = $stdin.read
-    stdin_info = check_stdin_info(stdin)
-    display_stdin_info(options, stdin_info)
+    files_contents << $stdin.read
   end
+
+  files_info = files_contents.map { |contents| check_file_info(contents) }
+  display_files_info(options, files_info, files_name)
+  display_total(options, files_info) if files_name.count > 1
 end
 
 def receive_argument
@@ -24,48 +26,29 @@ def receive_argument
   [options, ARGV]
 end
 
-def check_files_info(files)
-  files.map do |file|
-    hash = {}
-    hash[:name] = file
-    f = File.read(file)
-    hash[:lines] = f.lines.count
-    hash[:words] = f.split(/\s+/).count
-    hash[:bytes] = f.bytesize
-    hash
-  end
+def check_file_info(contents)
+  {
+    lines: contents.lines.count,
+    words: contents.split(/\s+/).count,
+    bytes: contents.bytesize
+  }
 end
 
-def check_stdin_info(stdin)
-  hash = {}
-  hash[:lines] = stdin.lines.count
-  hash[:words] = stdin.split(/\s+/).count
-  hash[:bytes] = stdin.bytesize
-  hash
-end
-
-def display_files_info(options, files)
-  files.each do |file|
+def display_files_info(options, files, files_name)
+  files.each_with_index do |file, i|
     print file[:lines].to_s.rjust(8)
     print file[:words].to_s.rjust(8) unless options[:l]
     print file[:bytes].to_s.rjust(8) unless options[:l]
-    print " #{file[:name]} "
+    print " #{files_name[i]} "
     puts ''
   end
 end
 
 def display_total(options, files)
-  print files.inject { |result, item| result[:lines] + item[:lines] }.to_s.rjust(8)
-  print files.inject { |result, item| result[:words] + item[:words] }.to_s.rjust(8) unless options[:l]
-  print files.inject { |result, item| result[:bytes] + item[:bytes] }.to_s.rjust(8) unless options[:l]
+  print files.map { |file| file[:lines] }.sum.to_s.rjust(8)
+  print files.map { |file| file[:words] }.sum.to_s.rjust(8) unless options[:l]
+  print files.map { |file| file[:bytes] }.sum.to_s.rjust(8) unless options[:l]
   print ' total'
-  puts ''
-end
-
-def display_stdin_info(options, stdin)
-  print stdin[:lines].to_s.rjust(8)
-  print stdin[:words].to_s.rjust(8) unless options[:l]
-  print stdin[:bytes].to_s.rjust(8) unless options[:l]
   puts ''
 end
 
